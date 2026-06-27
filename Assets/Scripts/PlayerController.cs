12a1ab1d43c8cd21d3dd32e503b8a9c5341389d3
Zomblack3 <santi260406@gmail.com>
2026-06-27 16:29:54 -0300

    Update gameplay
    Add player movement
    Set camera starting position
    Set player starting position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector2.up * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector2.down * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector2.left * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector2.right * Time.deltaTime);
     }
 }
